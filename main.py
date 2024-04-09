7441026237a8c20613d24f1dee3ade33050fd5c6
sys-griff <griffinromine29@gmail.com>
2024-04-09 14:04:04 -0700

    commit

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from pygame.locals import QUIT
 
 pygame.init()
 
-screen = pygame.display.set_mode((400, 300))
+screen = pygame.display.set_mode((1000, 800))
 
 screen.fill((255, 70, 231))
+
+clock = pygame.time.Clock()
 
 playerimg = pygame.image.load("Mario.png")
 coinimg = pygame.image.load("Coin Icon.png")
@@ -14,14 +16,25 @@ pygame.display.set_caption('Hello World!')
 coinimg = pygame.transform.scale(coinimg, (50, 50))
 playerimg = pygame.transform.scale(playerimg, (55, 60))
 
-coin = coinimg.get_rect(center = (200, 150))
-player = playerimg.get_rect(center = (50, 150))
+coin = coinimg.get_rect(center = (500, 400))
+player = playerimg.get_rect(center = (50, 400))
 
 while True:
+
+  clock.tick(240)
+
+  keys = pygame.key.get_pressed()
+
+  if keys[pygame.K_UP] or keys[pygame.K_w]:
+    player.y -= 2
+
   for event in pygame.event.get():
     if event.type == QUIT:
       pygame.quit()
       sys.exit()
+    
+  screen.fill((255, 70, 231))
+
   screen.blit(coinimg, coin)
   screen.blit(playerimg, player)
   
